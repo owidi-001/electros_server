48ddf81eb9bb8fd103b592847ddf6bfe5bd7167c
whoami <whoami@localhost.localdomain>
2022-06-22 01:49:25 +0300

    Heroku push files config

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from operator import mod
+from django.db import models
+
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    price=models.FloatField()
+    image=models.ImageField(upload_to="media/products")
---
+++ products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import product_list,product_detail
+
+urlpatterns=[
+    path('products/',product_list,name='products'),
+    path('products/<int:id>/',product_detail,name='product_details'),
+]
